af71c2f8abc40609935bdac31ee12290c9839a96
CuongPhan090 <vcuong0909@gmail.com>
2022-11-17 15:50:43 -0500

    Set up navigate up from detail page

--- app/src/main/java/com/example/rickandmortywiki/view/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortywiki/view/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.rickandmortywiki.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.rickandmortywiki.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = appBarConfig
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 }
