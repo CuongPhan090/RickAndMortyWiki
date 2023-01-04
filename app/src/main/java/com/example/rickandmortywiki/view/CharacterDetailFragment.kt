3ad83ee5a75b7f5dc43728c15025d44335ef2417
CuongPhan090 <vcuong0909@gmail.com>
2023-01-04 11:39:06 -0500

    Add transition animation between character detail fragments, and search fragment to detail fragment

--- app/src/main/java/com/example/rickandmortywiki/view/CharacterDetailFragment.kt
+++ app/src/main/java/com/example/rickandmortywiki/view/CharacterDetailFragment.kt
@@ -1,9 +1,16 @@
 package com.example.rickandmortywiki.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationSet
+import android.view.animation.DecelerateInterpolator
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -11,7 +18,6 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.rickandmortywiki.databinding.FragmentCharacterDetailBinding
-import com.example.rickandmortywiki.databinding.FragmentCharacterSearchBinding
 import com.example.rickandmortywiki.epoxy.uimodel.CharacterDetailsEpoxyController
 import com.example.rickandmortywiki.viewmodel.SharedViewModel
 import kotlinx.coroutines.launch
@@ -25,6 +31,7 @@ class CharacterDetailFragment : BaseFragment("Character Detail") {
     private val viewModel: SharedViewModel by viewModels()
     private val epoxyController = CharacterDetailsEpoxyController(::onClickEpisode)
     private val args: CharacterDetailFragmentArgs by navArgs()
+    private val handler: Handler = Handler(Looper.getMainLooper())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,11 +53,47 @@ class CharacterDetailFragment : BaseFragment("Character Detail") {
         viewModel.getCharacter(args.characterId.toString())
 
         binding?.epoxyRecyclerView?.setControllerAndBuildModels(epoxyController)
+
+        setUpAnimation()
+    }
+
+
+    private fun setUpAnimation() {
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val fadeOut = AlphaAnimation(1f, 0f).apply {
+                    interpolator = AccelerateInterpolator()
+                    duration = 250
+                }
+
+                val animation = AnimationSet(false).apply {
+                    addAnimation(fadeOut)
+                }
+                binding?.root?.animation = animation
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(onBackPressedCallback)
+
+        val fadeIn = AlphaAnimation(0f, 1f).apply {
+            interpolator = DecelerateInterpolator()
+            duration = 250
+        }
+
+        val animation = AnimationSet(false).apply {
+            addAnimation(fadeIn)
+        }
+        binding?.root?.animation = animation
     }
 
     private fun onClickEpisode(episodeId: Int?) {
         episodeId?.let {
-            findNavController().navigate(CharacterDetailFragmentDirections.actionCharacterDetailFragmentToEpisodeDetailBottomSheetFragment(it))
+            findNavController().navigate(
+                CharacterDetailFragmentDirections.actionCharacterDetailFragmentToEpisodeDetailBottomSheetFragment(
+                    it
+                )
+            )
         }
     }
 
